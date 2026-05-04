4eda7c7a63ff7e4378a630f34b30e11020837bec
yuinore <yuinore@hotmail.co.jp>
2026-05-04 20:38:45 +0900

    背景追加

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,23 @@
 import VideoEmbed from '@src/components/VideoEmbed';
+import './Root.scss';
 
 export default function Root() {
   return (
-    <div>
-      <h1 className="noto-sans-jp-thin">Hello World!</h1>
+    <div className="root-container">
+      <h1 className="noto-sans-jp-thin">幸せの青い鳥 BMS Package 特設ページ</h1>
       <VideoEmbed />
+      <h2>ダウンロード</h2>
+      <ul>
+        <li>ダウンロード 1</li>
+        <li>ダウンロード 1</li>
+        <li>ダウンロード 1</li>
+        <li>ダウンロード 1</li>
+        <li>ダウンロード 1</li>
+        <li>ダウンロード 1</li>
+        <li>ダウンロード 1</li>
+        <li>ダウンロード 1</li>
+        <li>ダウンロード 1</li>
+      </ul>
     </div>
   );
 }
