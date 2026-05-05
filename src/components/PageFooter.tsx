c0243f88e6502dbceb6b163218ff53befcd68d92
yuinore <yuinore@hotmail.co.jp>
2026-05-05 15:12:59 +0900

    シェアボタン追加

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -1,8 +1,35 @@
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  XIcon,
+  XShareButton,
+} from 'react-share';
+
 import './PageFooter.scss';
 
+const SHARE_TITLE = '幸せの青い鳥 BMS Package 特設ページ';
+
 export default function PageFooter() {
+  const shareUrl = typeof window !== 'undefined' ? window.location.href : '';
+
   return (
     <footer className="page-footer">
+      <div className="share-button" aria-label="このページをシェア">
+        <div className="share-network">
+          <XShareButton
+            url={shareUrl}
+            title={SHARE_TITLE}
+            className="share-network__button"
+          >
+            <XIcon size={24} round />
+          </XShareButton>
+        </div>
+        <div className="share-network">
+          <FacebookShareButton url={shareUrl} className="share-network__button">
+            <FacebookIcon size={24} round />
+          </FacebookShareButton>
+        </div>
+      </div>
       <p>Designed by yuinore / Team nemusugi.jp</p>
     </footer>
   );
